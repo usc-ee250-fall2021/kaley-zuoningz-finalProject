3b048f2613b87d559084a0337f43b083398302e7
fierykalechips <kaleytie@usc.edu>
2021-12-03 18:50:25 -0800

    Update rpiPubSub.py

--- rpiPubSub.py
+++ rpiPubSub.py
@@ -38,7 +38,7 @@ def lcd_callback(client, userdata, message):
         setText(txts[0][:15] + "\n" + txts[1][0:5] + "% RAM usage")
       #  setText('\n' + txts[1])
     
-    print(int(txts[1]))
+    print(txts)
     
     if int(txts[1]) > threshold:
         print("above threshold")
